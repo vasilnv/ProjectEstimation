c0f4221e6caa9d5ed326b2eb963679781daf632b
vasilnv <vasil.vasilev1999@abv.bg>
2021-06-13 22:05:30 +0300

    Add create a task functionality

---
+++ backend/api/tasks/create-task.php
@@ -0,0 +1,35 @@
+<?php
+require_once("../../db/db.php");
+$phpInput = json_decode(file_get_contents('php://input'), true);
+$projectName = $phpInput["project"];
+$name = $phpInput["name"];
+$description = $phpInput["description"];
+$estimation = $phpInput["estimation"];
+
+try {
+    $db = new DB();
+    $sql = "SELECT * FROM projects WHERE name = :projectname";
+    $connection = $db->getConnection();
+    $statement = $connection->prepare($sql);
+
+    $statement->execute(["projectname"=>$projectName]);
+    $project = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $projectId = $project[0]['id'];
+
+    if (sizeof($project) != 0) {
+        $sql = "INSERT INTO tasks (project, name, description, estimation) VALUES (:project, :name, :description, :estimation);";
+        $db = new DB();
+        $connection = $db->getConnection();
+        $statement = $connection->prepare($sql);
+        $statement->execute(["project"=>$projectId, "name" => $name, "description" => $description, "estimation" => $estimation]);
+
+        http_response_code(200);
+        echo json_encode(["status" => "SUCCESS", "message" => "Успешна регистрация", "statusCode" => 200]);
+    } else {
+        http_response_code(400);
+        echo json_encode(["status" => "ERROR", "message" => "Няма такъв проект", "statusCode" => 400]);
+    }
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(["status" => "ERROR", "message" => "Грешка при регистрация", "statusCode" => 500]);
+}
---
+++ frontend/tasks/task.js
@@ -0,0 +1,69 @@
+const btnCreate = document.getElementById('create-task-btn');
+const btnCancel = document.getElementById('cancel-btn');
+
+btnCreate.addEventListener('click', (event) => {
+    event.preventDefault();
+    var isValid = true;
+    if (document.getElementById("task_name").value.length < 3 || document.getElementById("task_name").value.length > 50){
+        document.getElementById('error_task_name').style.display = 'block'
+        document.getElementById('task_name').style.borderColor = '#B0706D'
+        document.getElementById('error_task_name').classList.remove("err_hidden");
+        document.getElementById('error_task_name').classList.add("error");
+        isValid = false;
+    } else {
+        document.getElementById('error_task_name').style.display = 'none'
+        document.getElementById('task_name').style.borderColor = '#C3CDC0'
+        document.getElementById('error_task_name').classList.remove('error')
+    }
+
+    //TODO check if a project exists and if not show error_project
+
+    if (document.getElementById("estimation").value < 0){
+        document.getElementById('error_estimation').style.display = 'block'
+        document.getElementById('estimation').style.borderColor = '#B0706D'
+        document.getElementById('error_estimation').classList.remove("err_hidden");
+        document.getElementById('error_estimation').classList.add("error");
+        isValid = false;
+    } else {
+        document.getElementById('error_estimation').style.display = 'none'
+        document.getElementById('estimation').style.borderColor = '#C3CDC0'
+        document.getElementById('error_estimation').classList.remove('error')
+    }
+
+    const formData = {
+        project: document.getElementById("project").value,
+        name: document.getElementById("task_name").value,
+        description: document.getElementById("description").value,
+        estimation: document.getElementById("estimation").value
+    };
+
+    console.log("here");
+
+    if (isValid) {
+        fetch('../../backend/api/tasks/create-task.php', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
+            .then(response=>response.json())
+            .then(response=> {
+                if(response.statusCode === 400) {
+                    document.getElementById("error_project").style.display='block';
+                } else if(response.statusCode === 500) {
+                    document.getElementById("error_server").style.display = 'block';
+                } else if (response.statusCode === 200) {
+                    window.location.replace("../index.html");
+                }
+            })
+            .then(data=>console.log(data));
+    }
+
+});
+
+btnCancel.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.replace("../index.html");
+});
