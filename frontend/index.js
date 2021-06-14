a9a6a20eaf68203bcf4a56da3e454925656671ab
VladislavStefanov <vladcata22@abv.bg>
2021-06-14 22:57:53 +0300

    Add create project functionality to index

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,37 @@ btnExit.addEventListener('click', (event) => {
 
 const btnProject = document.getElementById('create-project-btn');
 btnProject.addEventListener('click', (event) => {
-// TODO fetch to backend create a project
+    event.preventDefault();
+    var isValid = true;
+    debugger;
+    if (!document.getElementById("project-name").value.match(new RegExp("^[a-zA-Z0-9]{6,50}$"))){
+        document.getElementById('error_project_name').style.display = 'block'
+        document.getElementById('project-name').style.borderColor = '#B0706D'
+        document.getElementById('error_project_name').classList.remove("err_hidden");
+        document.getElementById('error_project_name').classList.add("error");
+        isValid = false;
+    } else {
+        document.getElementById('error_project_name').style.display = 'none'
+        document.getElementById('project-name').style.borderColor = '#C3CDC0'
+        document.getElementById('error_project_name').classList.remove('error')
+    }
+
+    const formData = {
+        projectName: document.getElementById("project-name").value
+    };
+
+    if (isValid) {
+
+        fetch('../backend/api/projects/create-project.php', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response=>response.json())
+            .then(data=>console.log(data));
+    }
 });
 
 const btnTask = document.getElementById('create-task-btn');
