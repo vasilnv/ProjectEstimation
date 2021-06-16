7c9e07e95e1f3530ae429d89205301e2be33a3e5
vasilnv <vasil.vasilev1999@abv.bg>
2021-06-17 00:13:28 +0300

    Add projectEstimation page

---
+++ frontend/projects/projectEstimated.js
@@ -0,0 +1,40 @@
+const projectId = new URLSearchParams(window.location.search).get("project");
+
+function refresh() {
+    fetch("../../backend/api/projects/get-project-name.php?project=" + projectId)
+        .then (response => response.json())
+        .then(json => {
+            document.getElementById("project-name").innerHTML = json.projectName;
+        })
+
+    fetch("../../backend/api/projects/get-project-manager.php?project=" + projectId)
+        .then (response => response.json())
+        .then(json => {
+            document.getElementById("managerName").innerHTML = json.managerName;
+        })
+
+    fetch("../../backend/api/projects/get-users-from-project.php?project=" + projectId)
+        .then (response => response.json())
+        .then(json => {
+            document.getElementById("devsNumber").innerHTML = json.numberOfUsers;
+            document.getElementById("coefficient").innerHTML = Math.log(json.totalCapacity/json.numberOfUsers);
+
+        })
+
+    fetch("../../backend/api/projects/get-tasks-from-project.php?project=" + projectId)
+        .then (response => response.json())
+        .then(json => {
+            document.getElementById("tasksNumber").innerHTML = json.numberOfTasks;
+        })
+
+    fetch("../../backend/api/projects/get-tasks-from-project.php?project=" + projectId)
+        .then (response => response.json())
+        .then(json => {
+            document.getElementById("estimationTime").innerHTML = json.estimatedProjectTime;
+        })
+
+
+
+}
+
+refresh();
