78aef867737241cabb1edca35388f0eca302a9b2
vasilnv <vasil.vasilev1999@abv.bg>
2021-06-17 00:13:17 +0300

    Add functionality to get total estimation of tasks in a project

--- backend/api/projects/get-tasks-from-project.php
+++ backend/api/projects/get-tasks-from-project.php
@@ -11,8 +11,21 @@ try {
     $statement->execute(["projectId" => $projectId]);
     $users = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+    $sql = "SELECT estimation FROM tasks WHERE project = :projectId";
+    $connection = $db->getConnection();
+
+    $statement = $connection->prepare($sql);
+    $statement->execute(["projectId" => $projectId]);
+    $estimations = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $projectTime =0;
+
+    for ($x = 0; $x < sizeof($users); $x++) {
+        $projectTime += $estimations[$x]["estimation"];
+    }
+
     http_response_code(200);
-    echo json_encode(["status" => "SUCCESS", "numberOfTasks" => sizeof($users)]);
+    echo json_encode(["status" => "SUCCESS", "numberOfTasks" => sizeof($users), "estimatedProjectTime" => $projectTime]);
 
 } catch (PDOException $e) {
     http_response_code(500);
