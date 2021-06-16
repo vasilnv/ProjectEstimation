ad6381abd0a4cc4d2ab1d8e9ab56294dc5c77e9d
vasilnv <vasil.vasilev1999@abv.bg>
2021-06-17 00:12:51 +0300

    Add functionality to get capacity of all users in a project

--- backend/api/projects/get-users-from-project.php
+++ backend/api/projects/get-users-from-project.php
@@ -11,10 +11,21 @@ try {
     $statement->execute(["projectId" => $projectId]);
     $users = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+    $totalCapacity=0;
+    for ($x = 0; $x < sizeof($users); $x++) {
+        $userId = $users[$x]["userId"];
+        $sql = "SELECT capacity FROM users WHERE id = :userId;";
+        $connection = $db->getConnection();
+
+        $statement = $connection->prepare($sql);
+        $statement->execute(["userId" => $userId]);
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+        $totalCapacity += $user["capacity"];
+    }
 
 
     http_response_code(200);
-    echo json_encode(["status" => "SUCCESS", "numberOfUsers" => sizeof($users)]);
+    echo json_encode(["status" => "SUCCESS", "numberOfUsers" => sizeof($users), "totalCapacity"=>$totalCapacity]);
 
 } catch (PDOException $e) {
     http_response_code(500);
