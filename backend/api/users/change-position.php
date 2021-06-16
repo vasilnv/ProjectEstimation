3722bcd141ab2bf6c0aba00876bcfb6db29da59a
System Administrator <root@Zhivkos-MacBook-Pro.local>
2021-06-17 00:29:14 +0300

    progress

--- backend/api/users/change-position.php
+++ backend/api/users/change-position.php
@@ -1,11 +1,26 @@
 <?php
+// session_start();
 require_once("../../db/db.php");
-$phpInput = json_decode(file_get_contents('php://input'), true);
 
-$position = $phpInput["position"];
-$userId = $phpInput["userId"];
 
 try {
+    $phpInput = json_decode(file_get_contents('php://input'), true);
+
+    $position = $phpInput["position"];
+    $userId = $phpInput["userId"];
+    //TODO not working pls help
+    // if (!$_SESSION["role"] || !$_SESSION["userId"]) {
+    //     http_response_code(403);
+    //     echo json_encode(["status" => "UNAUTHORIZED", "message" => "Липсват права!", "statusCode" => 403]);
+    //     exit();
+    // }
+
+    // if($_SESSION["role"] != "Admin" && $_SESSION["role"] != "Manager") {
+    //     http_response_code(403);
+    //     echo json_encode(["status" => "UNAUTHORIZED", "message" => "Липсват права!", "statusCode" => 403]);
+    //     exit();
+    // }
+
     $db = new DB();
     $sql = "UPDATE users set position=:position where id=:userId";
     $connection = $db->getConnection();
