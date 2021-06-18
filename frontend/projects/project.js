6ffc5a506f7ede6711ef17f51c4389b94e86e0c5
vasilnv <vasil.vasilev1999@abv.bg>
2021-06-19 01:43:14 +0300

    Fix refresh

--- frontend/projects/project.js
+++ frontend/projects/project.js
@@ -1,34 +1,6 @@
 const projectId = new URLSearchParams(window.location.search).get("project");
 
 function refresh() {
-    fetch("../../backend/api/tasks/get-all-tasks-for-project.php?project=" + projectId)
-        .then(response => response.json())
-        .then(json => {
-            var tasksElement = document.getElementById("project-tasks");
-            json.forEach(task => {
-                let li = document.createElement("li");
-                let estimationElement = document.createElement("h2");
-                estimationElement.innerHTML=`${task.estimation} дни`;
-                let nameElement = document.createElement("h3");
-                nameElement.innerHTML=`Име на задачата: ${task.name}`;
-                let descriptionElement = document.createElement("p");
-                descriptionElement.innerHTML=`Описание: ${task.description}`;
-                li.appendChild(estimationElement);
-                li.appendChild(nameElement);
-                li.appendChild(descriptionElement);
-                let btnDelete = document.createElement("button");
-                btnDelete.innerText = `Изтрий задача`;
-                li.appendChild(btnDelete);
-                tasksElement.appendChild(li);
-
-                btnDelete.addEventListener('click', (event) => {
-                        fetch("../../backend/api/tasks/delete-task.php?task=" + `${task.id}`)
-                            .then(response=>response.json)
-                            .then(data=>console.log(data));
-                    });
-                })
-            });
-
     fetch("../../backend/api/projects/get-project-name.php?project=" + projectId)
     .then (response => response.json())
     .then(json => {
@@ -90,4 +62,37 @@ function refresh() {
 
 }
 
+function refreshTasks() {
+    fetch("../../backend/api/tasks/get-all-tasks-for-project.php?project=" + projectId)
+        .then(response => response.json())
+        .then(json => {
+            var tasksElement = document.getElementById("project-tasks");
+            json.forEach(task => {
+                let li = document.createElement("li");
+                let estimationElement = document.createElement("h2");
+                estimationElement.innerHTML=`${task.estimation} дни`;
+                let nameElement = document.createElement("h3");
+                nameElement.innerHTML=`Име на задачата: ${task.name}`;
+                let descriptionElement = document.createElement("p");
+                descriptionElement.innerHTML=`Описание: ${task.description}`;
+                li.appendChild(estimationElement);
+                li.appendChild(nameElement);
+                li.appendChild(descriptionElement);
+                let btnDelete = document.createElement("button");
+                btnDelete.innerText = `Изтрий задача`;
+                li.appendChild(btnDelete);
+                tasksElement.appendChild(li);
+
+                btnDelete.addEventListener('click', (event) => {
+                    fetch("../../backend/api/tasks/delete-task.php?task=" + `${task.id}`)
+                        .then(response=>response.json)
+                        .then(data=>console.log(data));
+                });
+            })
+        });
+
+
+}
+
 refresh();
+refreshTasks();
