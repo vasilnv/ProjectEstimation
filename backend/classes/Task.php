ac73af8c3a5800251cc899f3f4cb85473c55e94c
vasilnv <vasil.vasilev1999@abv.bg>
2021-06-20 02:52:55 +0300

    Refactoring tasks

--- backend/classes/Task.php
+++ backend/classes/Task.php
@@ -3,5 +3,46 @@
 
 class Task
 {
+    public $id;
+    public $project;
+    public $name;
+    public $description;
+    public $estimation;
+
+    function __construct($id, $project, $name, $description, $estimation)
+    {
+        $this->id = $id;
+        $this->project = $project;
+        $this->name = $name;
+        $this->description = $description;
+        $this->estimation = $estimation;
+    }
+
+    public function createTask() {
+        $db = new DB();
+        $sql = "INSERT INTO tasks (project, name, description, estimation) VALUES (:project, :name, :description, :estimation);";
+        $connection = $db->getConnection();
+        $statement = $connection->prepare($sql);
+        $statement->execute(["project" => $this->project, "name" => $this->name, "description" => $this->description, "estimation" => $this->estimation]);
+    }
+
+    public function deleteTask() {
+        $db = new DB();
+        $sql = "DELETE FROM tasks WHERE id = :task";
+        $connection = $db->getConnection();
+        $statement = $connection->prepare($sql);
+
+        $statement->execute(["task"=>$this->id]);
+    }
+
+    public function getAllTasksForProject() {
+        $db = new DB();
+        $sql = "SELECT * FROM tasks WHERE project = :projectId";
+        $connection = $db->getConnection();
+        $statement = $connection->prepare($sql);
+        $statement->execute(["projectId" => $this->project]);
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    }
 
 }
