5ddce0b477b805dae6c570d1ec6c4ea96841b154
vasilnv <vasil.vasilev1999@abv.bg>
2021-06-20 20:15:57 +0300

    Fix db init

--- backend/db/db.php
+++ backend/db/db.php
@@ -1,13 +1,13 @@
 <?php
-
 class DB {
     private $connection;
 
     function __construct() {
-        $host = "localhost";
-        $name = "ProjectEstimation";
-        $username = "root";
-        $password = "";
+        $db_configs = parse_ini_file(__DIR__ . "/../../configs/db.ini");
+        $host = $db_configs['host'];
+        $name = $db_configs['name'];
+        $username = $db_configs['user'];
+        $password = $db_configs['pass'];
 
         $dsn = "mysql:host=$host; dbname=$name";
 
